ed7b3c8a151367dfa6f2f6fbc7d2ec50a8ba38eb
702161900 <fabrizio.cannizzo@bt.com>
2019-02-18 01:46:55 +0000

    introduced grid validation

--- src/test/kotlin/rb/robogrid/GridTest.kt
+++ src/test/kotlin/rb/robogrid/GridTest.kt
@@ -12,4 +12,15 @@ class GridTest {
         assertThat(g.zero, `is`(Position(0, 0)))
         assertThat(g.top, `is`(Position(5, 3)))
     }
+
+    @Test
+    fun `accepts a position and returns it if within boundaries`() {
+        assertThat(Grid(5, 3).accept(Position(2, 2)), `is`(Position(2, 2)))
+    }
+
+    @Test
+    fun `accepts a position and returns LOST if outside x or y boundaries`() {
+        assertThat(Grid(5, 3).accept(Position(10, 2)), `is`(Position.LOST))
+        assertThat(Grid(5, 3).accept(Position(3, 20)), `is`(Position.LOST))
+    }
 }
